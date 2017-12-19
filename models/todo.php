c94dcff8485e7cbd4ad9ed0af5a5a026b8dfdf47
michaelajala <mda9@njit.edu>
2017-12-18 19:38:28 -0500

    fixed all validation message redirect links

--- models/todo.php
+++ models/todo.php
@@ -18,6 +18,14 @@ final class todo extends database\model
         return $tableName;
     }
 
+    public function task_redirect(){
+
+        if ($this->id!=''){
+            echo' <a href="index.php?page=tasks&action=edit&id='. $this->id .'">Back</a><br>';
+        }else{
+            echo '<a href="index.php?page=tasks&action=create" >Back to the list</a><br>';
+        }
+    }
 
     public function validate()
     {
@@ -25,22 +33,17 @@ final class todo extends database\model
         $valid = TRUE;
         if ($this->message == '') {
             $valid = FALSE;
-            echo 'nothing in message<br/>';
-        if ($this->id!=''){
-            echo' <a href="index.php?page=tasks&action=edit&id='. $this->id .'">Back</a>';
-        }else{
-            ehco'<a href="index.php?page=tasks&action=edit>Back</a>';
-        }
+            echo 'Enter at least a character into the title field<br/>';
+            $this->task_redirect();
+
         }
 
         if (! ($this->isdone==1 || $this->isdone==0)){
         $valid = FALSE;
-        echo 'Enter 0 or 1<br/>';
-            echo' <a href="index.php?page=tasks&action=edit&id='. $this->id .'">Back</a>';
-
+             echo 'Enter 0 or 1<br/>';
+             $this->task_redirect();
         }
- 
- 
+
        return $valid;
 
     }
